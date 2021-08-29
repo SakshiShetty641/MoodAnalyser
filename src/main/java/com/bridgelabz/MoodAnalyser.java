ba5b1b519a9dd3b7870cc20bc552ad631c4eb276
Sakshi <sakshishetty1231997@gmail.com>
2021-08-29 22:15:30 +0530

    Added: Welcome message to the program

---
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+/**
+ * Purpose - To implement mood analyser program by handling custom exceptions
+ * @author - Sakshi Shetty
+ * @version - 16.0
+ * @since - 2021-08-29
+ */
+public class MoodAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the program");
+    }
+}
